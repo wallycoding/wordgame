13ee8a29f0829326212ee28a9d4b5dbd9063ffce
wallycoding <wallysonbreno.dev@gmail.com>
2023-05-29 18:59:54 -0300

    fix: min-screen

--- src/app/pages/Auth/index.tsx
+++ src/app/pages/Auth/index.tsx
@@ -4,7 +4,7 @@ import Providers from "./components/Providers";
 
 const Auth = () => {
   return (
-    <div className="grid min-h-screen w-full place-items-center">
+    <div className="grid min-h-[100svh] w-full place-items-center">
       <form className="flex w-full max-w-md flex-col gap-14 border-1 border-night-300 bg-night-auth px-8 py-12 backdrop-blur-lg big-sm:rounded-lg">
         <div className="flex items-center justify-between">
           <h1 className="text-stroke-white text-5xl font-black">Sign In</h1>
--- src/app/pages/Game/index.tsx
+++ src/app/pages/Game/index.tsx
@@ -6,7 +6,7 @@ const Game = async () => {
   const words = await randomWords();
 
   return (
-    <main className="flex min-h-screen justify-center">
+    <main className="flex min-h-[100svh] justify-center">
       <div className="my-4 flex w-full max-w-4xl flex-col gap-3 overflow-auto">
         <Gamepad words={words} />
       </div>
