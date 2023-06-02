e1fa08edc8519e8b6ba50dea05f7e6f6bcf93d02
wallycoding <wallysonbreno.dev@gmail.com>
2023-06-01 23:51:25 -0300

    fix Root session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,5 @@ export default //
 async function Root(props: any) {
   const session = await getServerSession();
   // @ts-ignore
-  // return session ? <Game /> : <Auth />;
-  return <Game />;
+  return session ? <Game /> : <Auth />;
 }
